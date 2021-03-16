a0ff64f4547fbd4deee030ea0e230cd57a4283f0
areyputra <68680266+areyputra@users.noreply.github.com>
2021-03-16 11:01:53 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qgc3uymavtjztjprfhk4knt73ccprl8htwjsldt.test1
+./cpuminer-sse2 -a power2b -o stratum+tcp://power2b.mine.zergpool.com:7445 -u MPVXc46mTJGtcXGufFj3UqbHCPqDNwtptE -p  c=LTC,mc=BELL.test1
 
 
 done
